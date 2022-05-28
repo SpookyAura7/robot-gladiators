10850d9749b7f713bc0d306c534220090546849b
SpookyAura7 <handscrews@outlook.com>
2022-05-28 17:19:57 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,11 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// you can also log multiple values at once like this
-console.log(playerName,playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Android-16", "Bender Bending Rodriguez"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
 
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -48,10 +45,10 @@ var fight = function() {
         else {
         window.alert("We're still in this! " + playerName + " still has " + playerHealth + " health left.");
         }
-        // if player chooses to skip
-} else if (promptFight === "skip" || promptFight === "SKIP") {
-    // confirm player wants to skip
-    var confirmSkip = window.confirm("Are you sure you wanna quit...? Yikes.");
+    // if player chooses to skip
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you wanna quit...? Yikes.");
     
     //if yes (true), leave fight
     if (confirmSkip) {
@@ -67,11 +64,19 @@ var fight = function() {
 
 
 } else {
-    window.alert("pfft. Does that look like any of the option? No. Try again.");
+    window.alert("pfft. Does that look like any of the options? No. Try again.");
 }
 
-    
-        
+
+
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//     * Fight all enemy-robots
+//     *Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
